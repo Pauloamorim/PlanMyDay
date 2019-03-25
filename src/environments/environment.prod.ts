d85243b5d7563d0a280f844f9ab2c38a6b8a3edd
Paulo Amorim <pauloamoriim@gmail.com>
2019-03-25 20:30:20 -0300

    changes to deploy in heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: "AIzaSyDV9BaluVViBTKkC7cUfkjMUR53PO1ddRk",
+    authDomain: "planmydayapp-80d7c.firebaseapp.com",
+    databaseURL: "https://planmydayapp-80d7c.firebaseio.com",
+    projectId: "planmydayapp-80d7c",
+    storageBucket: "planmydayapp-80d7c.appspot.com",
+    messagingSenderId: "541849808781"
+  }
 };
